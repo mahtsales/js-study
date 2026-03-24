311a4c8f5a39ff7d9f58cbc78a13c625525af024
mahtsales <davimatheusfsales@gmail.com>
2026-03-23 23:18:40 -0400

    Adding the prompt-sync tool to complete the exercise.

---
+++ atv1.js
@@ -0,0 +1,12 @@
+//ESTUDANDO UM POUCO SOBRE JAVASCRIPT!
+
+const prompt = require("prompt-sync")();
+let salario = Number(prompt("Digite o valor do salário: "));
+
+if(salario >= 600 && salario <= 900){
+    console.log("Seu salário é de menor aprendiz.");
+}else if(salario > 900 && salario <= 1500){
+    console.log("Seu salário é de CLT comum.");
+}else{
+    console.log("Você é vagabundo.");
+};
