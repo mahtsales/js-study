fd04302c52920f760d05c9dccf43c1b8e5145080
mahtsales <davimatheusfsales@gmail.com>
2026-03-24 11:02:56 -0400

    Fix: Fix bugs and reinstalling prompt-sync

---
+++ variaveis-e-operadores/atv3.js
@@ -0,0 +1,9 @@
+//Realizando operaçao booleana com numeros impares ou pares
+
+const prompt = require ("prompt-sync")();
+let numero = Number(prompt("Digite o número que deseja verificar: "));
+if (numero % 2 === 0){
+    console.log(`O número ${numero} é par.`);
+}else{
+    console.log(`O número ${numero} é ímpar.`);
+};
