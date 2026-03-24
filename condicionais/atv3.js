369a80448952ae963b9671e1c221c73886c3ed8e
mahtsales <davimatheusfsales@gmail.com>
2026-03-24 00:13:15 -0400

    Practicing conditional statements and boolean logic.

---
+++ condicionais/atv3.js
@@ -0,0 +1,10 @@
+// Um pouco sobre boolean e condicionais
+
+const prompt = require("prompt-sync")();
+
+const matricula = String(prompt("Você possui nº de matrícula?: "));
+if(alunoMatriculado.toLowerCase() === "sim"){
+    console.log("Aluno matriculado.");
+}else{
+    console.log("Aluno não matriculado.");
+};
