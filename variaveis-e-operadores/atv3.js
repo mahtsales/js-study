bf342b0a4abe942fd8aef41a33cc71df4e8bea0e
mahtsales <davimatheusfsales@gmail.com>
2026-03-24 11:22:13 -0400

    Fix: Corrections in the if-else exercise.

--- variaveis-e-operadores/atv3.js
+++ variaveis-e-operadores/atv3.js
@@ -2,7 +2,7 @@
 
 const prompt = require ("prompt-sync")();
 let numero = Number(prompt("Digite o número que deseja verificar: "));
-if (numero % 2 === 0){
+if ((numero % 2) === 0){
     console.log(`O número ${numero} é par.`);
 }else{
     console.log(`O número ${numero} é ímpar.`);
