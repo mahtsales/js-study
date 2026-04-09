b720a860964e48c03a2e25594a6f276b8222523c
mahtsales <davimatheusfsales@gmail.com>
2026-04-08 23:35:29 -0400

    feat: arrays and for method exercise

---
+++ arrays(listas)/atv1.js
@@ -0,0 +1,15 @@
+const alunos = ["Davi", "Matheus", "Ferreira", "Sales"];
+
+const notas = [];
+notas.push(8.5);
+notas.push(7.5);
+notas.push(8.5);
+notas.push(9.5);
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / 4;
+
+for (let index = 0; index < array.length; index++) {
+   
+}
+
+console.log(media.toFixed(2));
